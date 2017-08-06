aee6021fa902ca90b0d2b07d107cc22678d63d08
Abhijit <soman.abhijit@gmail.com>
2017-08-06 13:37:02 -0400

    one more solution for problem 5

--- src/racket/P1-28/P05.rkt
+++ src/racket/P1-28/P05.rkt
@@ -6,8 +6,21 @@
       '()
       (append (reverse (rest lst)) (list (first lst)))))
 
+;; Another solution
+(define (reverse2 lst)
+  (define (reverse-rec lst r)
+    (if (empty? lst)
+      r
+      (reverse-rec (rest lst) (cons (first lst) r))))
 
-; Tests
+  (reverse-rec lst '()))
+
+
+;; Tests
 (equal? (reverse '(a b c d e f g h)) '(h g f e d c b a))
 (equal? (reverse '(123.56 a "spam" ("egg" 45))) '(("egg" 45) "spam" a 123.56))
 (equal? (reverse '()) '())
+
+(equal? (reverse2 '(a b c d e f g h)) '(h g f e d c b a))
+(equal? (reverse2 '(123.56 a "spam" ("egg" 45))) '(("egg" 45) "spam" a 123.56))
+(equal? (reverse2 '()) '())
