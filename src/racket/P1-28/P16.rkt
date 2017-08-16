119d55ffa6101e81cb205d6fd6f36c5a081acda2
Abhijit <soman.abhijit@gmail.com>
2017-08-16 19:18:15 -0400

    Solution for P16

---
+++ src/racket/P1-28/P16.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+
+;; Drop every N'th element from a list.
+;; Example: (drop '(a b c d e f g h i k) 3) => '(a b d e g h k)
+
+(define (drop lst n)
+  (define (drop-rec lst i)
+    (if (empty? lst)
+        '()
+        (if (= (remainder i n) 0)
+            (drop-rec (rest lst) (+ i 1))
+            (cons (first lst) (drop-rec (rest lst) (+ i 1))))))
+
+  (drop-rec lst 1))
+
+
+;; Tests
+(equal? (drop '(a b c d e f g h i k) 3) '(a b d e g h k))
+(equal? (drop '(a b c d e f g h i k) 2) '(a c e g i))
+(equal? (drop '(a b c d e) 1) '())
