d13a4ffb189bba0d07ee5efd170c61ff348d2d71
Abhijit <soman.abhijit@gmail.com>
2017-08-17 17:43:00 -0400

    Added another solution for P16

--- src/racket/P1-28/P16.rkt
+++ src/racket/P1-28/P16.rkt
@@ -13,8 +13,23 @@
 
   (drop-rec lst 1))
 
+;; A better solution
+(define (drop2 lst pos)
+  (define (drop-rec lst i)
+    (cond
+      [(empty? lst) '()]
+      [(= i 1) (drop-rec (rest lst) pos)]
+      [else (cons (first lst) (drop-rec (rest lst) (- i 1)))]))
+
+  (drop-rec lst pos))
+
 
 ;; Tests
 (equal? (drop '(a b c d e f g h i k) 3) '(a b d e g h k))
 (equal? (drop '(a b c d e f g h i k) 2) '(a c e g i))
 (equal? (drop '(a b c d e) 1) '())
+
+(equal? (drop2 '(a b c d e f g h i k) 3) '(a b d e g h k))
+(equal? (drop2 '(a b c d e f g h i k) 2) '(a c e g i))
+(equal? (drop2 '(a b c d e) 1) '())
+
